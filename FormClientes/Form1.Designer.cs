38cbf229922b48a16361b2d37edd0dbd833919c6
Lucas <Lucas@DESKTOP-I3M1LSV>
2025-05-10 19:05:35 -0300

    Actualizaciones en cliente y consumo de API

--- FormClientes/Form1.Designer.cs
+++ FormClientes/Form1.Designer.cs
@@ -28,12 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dgvCliente = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtNombre = new TextBox();
+            txtApellido = new TextBox();
+            txtId = new TextBox();
+            btn_agregar = new Button();
+            btn_modificar = new Button();
+            btn_eliminar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvCliente).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvCliente
+            // 
+            dgvCliente.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvCliente.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvCliente.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCliente.Location = new Point(12, 100);
+            dgvCliente.Name = "dgvCliente";
+            dgvCliente.ReadOnly = true;
+            dgvCliente.Size = new Size(734, 185);
+            dgvCliente.TabIndex = 0;
+            dgvCliente.SelectionChanged += dgvAlumnos_SelectionChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(251, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 18);
+            label1.TabIndex = 1;
+            label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Tahoma", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(526, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 18);
+            label2.TabIndex = 2;
+            label2.Text = "Apellido:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Tahoma", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(14, 63);
+            label3.Name = "label3";
+            label3.Size = new Size(27, 18);
+            label3.TabIndex = 4;
+            label3.Text = "Id:";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(322, 63);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(149, 23);
+            txtNombre.TabIndex = 5;
+            // 
+            // txtApellido
+            // 
+            txtApellido.Location = new Point(597, 63);
+            txtApellido.Name = "txtApellido";
+            txtApellido.Size = new Size(149, 23);
+            txtApellido.TabIndex = 6;
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(47, 61);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(149, 23);
+            txtId.TabIndex = 8;
+            // 
+            // btn_agregar
+            // 
+            btn_agregar.Location = new Point(769, 100);
+            btn_agregar.Name = "btn_agregar";
+            btn_agregar.Size = new Size(99, 42);
+            btn_agregar.TabIndex = 9;
+            btn_agregar.Text = "AGREGAR";
+            btn_agregar.UseVisualStyleBackColor = true;
+            btn_agregar.Click += btnAgregar_Click;
+            // 
+            // btn_modificar
+            // 
+            btn_modificar.Location = new Point(769, 169);
+            btn_modificar.Name = "btn_modificar";
+            btn_modificar.Size = new Size(99, 42);
+            btn_modificar.TabIndex = 10;
+            btn_modificar.Text = "MODIFICAR";
+            btn_modificar.UseVisualStyleBackColor = true;
+            btn_modificar.Click += btnActualizar_Click;
+            // 
+            // btn_eliminar
+            // 
+            btn_eliminar.Location = new Point(769, 243);
+            btn_eliminar.Name = "btn_eliminar";
+            btn_eliminar.Size = new Size(99, 42);
+            btn_eliminar.TabIndex = 11;
+            btn_eliminar.Text = "ELIMINAR";
+            btn_eliminar.UseVisualStyleBackColor = true;
+            btn_eliminar.Click += btnEliminar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(880, 330);
+            Controls.Add(btn_eliminar);
+            Controls.Add(btn_modificar);
+            Controls.Add(btn_agregar);
+            Controls.Add(txtId);
+            Controls.Add(txtApellido);
+            Controls.Add(txtNombre);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dgvCliente);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvCliente).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dgvCliente;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtNombre;
+        private TextBox txtApellido;
+        private TextBox txtId;
+        private Button btn_agregar;
+        private Button btn_modificar;
+        private Button btn_eliminar;
     }
 }
--- FormClientes/Form1.cs
+++ FormClientes/Form1.cs
@@ -1,10 +1,153 @@
+using LibClassModels.Modelos;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json;
+using System.Windows.Forms;
+
 namespace FormClientes
 {
     public partial class Form1 : Form
     {
+        private readonly string apiUrl = "http://localhost:5069";
+        private readonly HttpClient httpClient = new HttpClient();
+        private List<Cliente> listaCliente = new();
+
         public Form1()
         {
             InitializeComponent();
+            dgvCliente.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvCliente.MultiSelect = false;
         }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await CargarClientesAsyncs();
+        }
+
+        private async Task CargarClientesAsyncs()
+        {
+            try
+            {
+                var response = await httpClient.GetAsync($"{apiUrl}/api/Cliente");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+
+                    // Usar Newtonsoft para manejar herencia
+                    listaCliente = JsonConvert.DeserializeObject<List<Cliente>>(json, new JsonSerializerSettings
+                    {
+                        TypeNameHandling = TypeNameHandling.All
+                    });
+
+                    dgvCliente.AutoGenerateColumns = true;
+                    dgvCliente.DataSource = listaCliente.Select(c => new
+                    {
+                        c.Id,
+                        c.Nombre,
+                        c.Apellido,
+                        Tipo = c.GetType().Name
+                    }).ToList();
+                }
+                else
+                {
+                    MessageBox.Show($"Error al cargar los clientes. Código: {(int)response.StatusCode} ({response.StatusCode})");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar los clientes: {ex.Message}");
+            }
+        }
+
+        private async void btnAgregar_Click(object sender, EventArgs e)
+        {
+            var cliente = new ClienteRequestPost
+            {
+                id = Convert.ToInt32(txtId.Text),
+                Nombre = txtNombre.Text,
+                Apellido = txtApellido.Text,
+                Tipo = "Frecuente" // podés cambiar esto si usás un ComboBox para el tipo
+            };
+
+            var json = JsonConvert.SerializeObject(cliente);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PostAsync($"{apiUrl}/api/Cliente", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Cliente agregado correctamente");
+                await CargarClientesAsyncs();
+            }
+            else
+            {
+                MessageBox.Show("Error al agregar el cliente");
+            }
+        }
+
+        private async void btnActualizar_Click(object sender, EventArgs e)
+        {
+            if (dgvCliente.CurrentRow == null) return;
+
+            var cliente = listaCliente[dgvCliente.CurrentRow.Index];
+            cliente.Nombre = txtNombre.Text;
+            cliente.Apellido = txtApellido.Text;
+
+            var json = JsonConvert.SerializeObject(cliente, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.All
+            });
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PutAsync($"{apiUrl}/api/Cliente/{cliente.Id}", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Cliente actualizado correctamente");
+                await CargarClientesAsyncs();
+            }
+            else
+            {
+                MessageBox.Show("Error al actualizar el cliente");
+            }
+        }
+
+        private async void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvCliente.CurrentRow == null) return;
+
+            var cliente = listaCliente[dgvCliente.CurrentRow.Index];
+            var response = await httpClient.DeleteAsync($"{apiUrl}/api/Cliente/{cliente.Id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Cliente eliminado correctamente");
+                await CargarClientesAsyncs();
+            }
+            else
+            {
+                MessageBox.Show("Error al eliminar el cliente");
+            }
+        }
+
+        private void dgvAlumnos_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvCliente.CurrentRow == null || listaCliente.Count == 0) return;
+
+            var cliente = listaCliente[dgvCliente.CurrentRow.Index];
+
+            txtId.Text = cliente.Id.ToString();
+            txtNombre.Text = cliente.Nombre;
+            txtApellido.Text = cliente.Apellido;
+        }
+    }
+
+    public class ClienteRequestPost
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Tipo { get; set; }
     }
 }
